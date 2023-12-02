75b6f194f27bb3390ec1524d9b5cc428fc7bfa35
Johann3s Höß <johanneshoess@r3leaf.earth>
2023-12-02 01:56:18 +0100

    add richtext editor

--- schnitz3l/settings.py
+++ schnitz3l/settings.py
@@ -92,7 +92,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'crispy_bootstrap4'
+    'crispy_bootstrap4',
+    'pagedown.apps.PagedownConfig'
 ]
 
 # Crispy Forms Template
--- vtanz/admin.py
+++ vtanz/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.db import models
 from .models import Article
+from pagedown.widgets import AdminPagedownWidget
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': AdminPagedownWidget},
+    }
 
 # Register your models here.
 admin.site.register(Article)
