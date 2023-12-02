aed2c0a76c0badba929a46d80146d153c4cdcda2
Johann3s Höß <johanneshoess@r3leaf.earth>
2023-12-02 01:01:18 +0100

    feature(richtext) markdown in vtanz

--- vtanz/views.py
+++ vtanz/views.py
@@ -1,6 +1,12 @@
 from django.views import generic
 from .models import Article
 from django.shortcuts import render
+import markdown
+
+
+def convert_markdown(article):
+    md = markdown.Markdown(extensions=["fenced_code"])
+    article.text = md.convert(article.text)
 
 
 def IndexView(request):
@@ -9,6 +15,7 @@ def IndexView(request):
     for article in article_list:
         article.pos = 'right' if pos % 2 else 'left'
         pos += 1
+        convert_markdown(article)
     return render(request, 'vtanz/index.html', {'article_list': article_list})
 
 
@@ -18,6 +25,7 @@ def ArchiveView(request):
     for article in article_list:
         article.pos = 'right' if pos % 2 else 'left'
         pos += 1
+        convert_markdown(article)
     return render(request, 'vtanz/archive.html', {'article_list': article_list})
 
 
